12a1656bacd6cb759b9389a52d9f635cd14cea46
David Deller <david@horizon-nigh.org>
2021-06-08 04:16:56 +0000

    Update to latest ytdl-core
    Fixes brokenness

--- index.js
+++ index.js
@@ -5,42 +5,40 @@ const ytdl = require('ytdl-core')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-function sendToStream(videoID, response) {
+async function sendToStream(videoID, response) {
   let url = "https://www.youtube.com/watch?v=" + videoID
-  ytdl.getInfo(url, (err, info) => {
-    if (err) throw err
+  let info = await ytdl.getInfo(url)
+  
+  // console.log(info)
+  
+  let streamURL = info.player_response.streamingData.hlsManifestUrl
+  
+  if (!!streamURL) {
+    response.redirect(302, streamURL)
     
-//     console.log(info)
-    
-    let streamURL = info.player_response.streamingData.hlsManifestUrl
-    
-    if (!!streamURL) {
-      response.redirect(302, streamURL)
-      
-    } else {
-      console.log("Could not find stream URL in data:", info)
-      response.status(500).send("Unable to get stream URL")
-    }
-  })
+  } else {
+    console.log("Could not find stream URL in data:", info)
+    response.status(500).send("Unable to get stream URL")
+  }
 }
 
-app.get('/relax_study', (request, response) => {
+app.get('/relax_study', async (request, response) => {
   let videoID = "5qap5aO4i9A"
-  sendToStream(videoID, response)
+  await sendToStream(videoID, response)
 })
 
-app.get('/sleep_chill', (request, response) => {
+app.get('/sleep_chill', async (request, response) => {
   let videoID = "DWcJFNfaw9c"
-  sendToStream(videoID, response)
+  await sendToStream(videoID, response)
 })
 
-app.get('/stream', (request, response) => {
+app.get('/stream', async (request, response) => {
   let videoID = request.query.video_id
   if (!videoID) {
     response.status(400).send("Specify a YouTube video ID with ?video_id=...")
     return
   }
-  sendToStream(videoID, response)
+  await sendToStream(videoID, response)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
